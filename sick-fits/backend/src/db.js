9f2b5fa1f03806d7b4cb934112de44487605ae4e
sherwino <sherwino@outlook.com>
2019-04-13 17:25:22 -0400

    Set up Prisma Server, and GraphQL Connection

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,12 @@
+// This file connects to the remote prisma db, and lets us query it with js
+// Node doesn't have import yet
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+
+// TODO: Use express to handle cookies JWT
+// TODO: Use express to populate current user 
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL
+        },
+    }, 
+    deets => {
+        console.log(`Server is now running on port ${deets.port}`);
+    }
+);
