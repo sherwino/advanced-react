5cb1dfb2ad672071487b8ca8fc3feb9752cd9242
sherwino <sherwino@outlook.com>
2019-05-14 23:03:41 -0400

    Item creation using prisma yoga (14)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        
+        return item;
+    }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+
+    //     const newDog = { name: args.name };
+    //     global.dogs.push(newDog);
+
+    //     console.log('args', args);
+    //     return newDog;
+    // },
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,19 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    // forwardto lets you just push an pull data to your db without writing
+    // a query like the ones below, but if you want to do auth or antyhing like that
+    // you should still manually write the query
+    items: forwardTo('db')
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+
+    //     return items;
+    // },
+    // dogs(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     return global.dogs;
+    // },
+};
 
 module.exports = Query;
